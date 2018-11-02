882ad2485a35eb069be1c14b06bed37bf532d11b
Alex Zurad <az15hh@brocku.ca>
2018-11-02 17:29:33 -0400

    Initial repository setup.

---
+++ src/assign_2/Vector.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assign_2;
+
+/**
+ *
+ * @author Alex Zurad
+ */
+public class Vector {
+    
+    public Vector(){
+        
+    }
+    
+    public Vector(int size, double d){
+        
+    }
+    
+    public Vector(double[] d){
+        
+    }
+    
+    public Vector append(){
+        return null;
+    }
+    
+}
