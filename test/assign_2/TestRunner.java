c3a1eac1975eb96708fa1dfc09fb24da4501e364
Alex Zurad <az15hh@brocku.ca>
2018-11-10 21:45:29 -0500

    Added exceptions

--- test/assign_2/TestRunner.java
+++ test/assign_2/TestRunner.java
@@ -9,10 +9,15 @@ import org.junit.runner.notification.Failure;
 
 public class TestRunner {				
 			public static void main(String[] args) {									
-      Result result = JUnitCore.runClasses(VectorTest.class);					
-			for (Failure failure : result.getFailures()) {							
-         System.out.println(failure.toString());					
-      }		
-      System.out.println("Result=="+result.wasSuccessful());							
-   }		
+//      Result result = JUnitCore.runClasses(VectorTest.class);					
+//			for (Failure failure : result.getFailures()) {							
+//         System.out.println(failure.toString());					
+//      }		
+//      System.out.println("Result=="+result.wasSuccessful());							
+//   }	
+
+
+ Vector b = null;
+ b.getLength();
+         }
 }     
